14fc908197890c49138352f9b643cc32ec71e5f4
NguyenTungSon0503 <sonmeliodas0503@gmail.com>
2023-11-18 19:20:20 +0700

    update

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -2,7 +2,7 @@ const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 const path = require('path');
 
-const PROTO_PATH = path.join(__dirname, '../../proto/helloworld.proto');
+const PROTO_PATH = path.join(__dirname, '../proto/helloworld.proto');
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
   longs: String,
